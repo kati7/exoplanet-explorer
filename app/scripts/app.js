5db0cf55a430534d3dc32a61d2a56b8b77173b49
Katarzyna Osinska <kati.zapart@gmail.com>
2019-03-25 10:22:34 +0100

    feat: Bonus feauture

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,17 +21,33 @@ proper order even if all the requests haven't finished.
     home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
+  // /**
+  //  * Helper function to create a planet thumbnail.
+  //  * @param  {Object} data - The raw data describing the planet.
+  //  */
+  // function createPlanetThumb(data) {
+  //   var pT = document.createElement('planet-thumb');
+  //   for (var d in data) {
+  //     pT[d] = data[d];
+  //   }
+  //   home.appendChild(pT);
+  // }
+
   /**
-   * Helper function to create a planet thumbnail.
-   * @param  {Object} data - The raw data describing the planet.
-   */
-  function createPlanetThumb(data) {
+ * Helper function to create a planet thumbnail - Promisified version!
+ * @param  {Object} data - The raw data describing the planet.
+ */
+function createPlanetThumb(data) {
+  return new Promise(function(resolve) {
     var pT = document.createElement('planet-thumb');
     for (var d in data) {
       pT[d] = data[d];
     }
     home.appendChild(pT);
-  }
+    console.log('rendered: ' + data.pl_name);
+    resolve();
+  });
+}
 
   /**
    * XHR wrapped in a promise
@@ -42,22 +58,88 @@ proper order even if all the requests haven't finished.
     return fetch(url);
   }
 
+  // /**
+  //  * Performs an XHR for a JSON and returns a parsed JSON response.
+  //  * @param  {String} url - The JSON URL to fetch.
+  //  * @return {Promise}    - A promise that passes the parsed JSON response.
+  //  */
+  // function getJSON(url) {
+  //   return get(url).then(function(response) {
+  //     return response.json();
+  //   });
+  // }
+
   /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
-   * @param  {String} url - The JSON URL to fetch.
-   * @return {Promise}    - A promise that passes the parsed JSON response.
-   */
-  function getJSON(url) {
-    return get(url).then(function(response) {
+ * Performs an XHR for a JSON and returns a parsed JSON response - with a delay!
+ * @param  {String} url - The JSON URL to fetch.
+ * @return {Promise}    - A promise that passes the parsed JSON response.
+ */
+function getJSON(url) {
+  console.log('sent: ' + url);
+  return get(url).then(function(response) {
+    // For testing purposes, I'm making sure that the urls don't return in order
+    if (url === 'data/planets/Kepler-62f.json') {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          console.log('received: ' + url);
+          resolve(response.json());
+        }, 500);
+      });
+    } else {
+      console.log('received: ' + url);
       return response.json();
-    });
-  }
+    }
+  });
+}
+
+
+  // window.addEventListener('WebComponentsReady', function() {
+  //   home = document.querySelector('section[data-route="home"]');
+  //   /*
+  //   Your code goes here!
+  //    */
+  //   getJSON('../data/earth-like-results.json')
+  //   .then(function(response) {
+  //     console.log(response);
+  //     return response.results.map(getJSON)
+  //      .reduce(function(sequence, planetPromise) {
+  //        return sequence.then(function() {
+  //          return planetPromise;
+  //        }).then(function(planetData) {
+  //          createPlanetThumb(planetData);
+  //        });
+  //      }, Promise.resolve());
+  //   }).catch(function(error) {
+  //     console.log(error);
+  //   });
+  // });
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Your code goes here!
-     */
-    // getJSON('../data/earth-like-results.json')
+  
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      addSearchHeader(response.query);
+      return response;
+    })
+    .then(function(response) {
+      var sequence = Promise.resolve();
+  
+      // .map executes all of the network requests immediately.
+      var arrayOfExecutingPromises = response.results.map(function(result) {
+        return getJSON(result);
+      });
+  
+      arrayOfExecutingPromises.forEach(function(request) {
+        // Loop through the pending requests that were returned by .map (and are in order) and
+        // turn them into a sequence.
+        // request is a getJSON() that's currently executing.
+        sequence = sequence.then(function() {
+          // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+          // later in the sequence can execute.
+          return request.then(createPlanetThumb);
+        });
+      });
+    });
   });
 })(document);
